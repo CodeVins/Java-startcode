8472d78370a871692570006cc1f650d7e0b2f167
CodeVins <dhy9987@naver.com>
2024-07-31 16:16:09 +0900

    suika-game
    This game is a physics-based puzzle game where fruits drop from the top of the screen, navigating various obstacles to reach the bottom. Players can control the path of the fruits or drop additional fruits using a mouse or touchscreen. The game features a bright and lively background with fun fruit images, enhancing the overall enjoyment. Each fruit has unique properties and bounce effects, requiring players to strategically drop the fruits to achieve the best results.

---
+++ suika-game/main.js
@@ -0,0 +1,59 @@
+import { Engine, Render, Runner, Bodies, World } from "matter-js";
+import { FRUITS } from "./fruits";
+const engine = Engine.create(); 
+const render = Render.create({
+  engine,
+  element: document.body,
+  options: {
+    wireframes: false,
+    background: "#F7F4C8",
+    width: 620,
+    height: 850,
+  }
+});
+
+const world = engine.world;
+
+const leftWall = Bodies.rectangle(15, 395, 30, 790, {
+  isStatic: true,
+  render: {fillStyle: "#E6B143" }
+});
+
+const rightWall = Bodies.rectangle(605, 395, 30, 790, {
+  isStatic: true,
+  render: {fillStyle: "#E6B143" }
+});
+
+const ground = Bodies.rectangle(310, 820, 620, 60, {
+  isStatic: true,
+  render: {fillStyle: "#E6B143" }
+});
+
+const topLine = Bodies.rectangle(310, 150, 620, 2, {
+  isStatic: true,
+  isSensor: true,
+  render: {fillStyle: "#E6B143"}
+});
+
+World.add(world, [leftWall, rightWall, ground, topLine]);
+
+Render.run(render);
+Runner.run(engine);
+
+function addFruit() {
+  const index = 7;
+  const fruit = FRUITS[index];
+
+  const body = Bodies.circle(300, 50, fruit.radius, {
+    index: index,
+    render: {
+      sprite: { texture: `${fruit.name}.png` }
+    },
+    restitution: 0.2
+  });
+
+  World.add(world, body);
+
+}
+
+addFruit();
